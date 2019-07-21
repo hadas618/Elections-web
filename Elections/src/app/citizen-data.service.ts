f6f5071a20a75e896eb146eb033c8ea3d4f025d3
hadas618 <hadas618@gmail.com>
2019-07-21 22:17:15 +0300

    twenty one commit

--- Elections/src/app/citizen-data.service.ts
+++ Elections/src/app/citizen-data.service.ts
@@ -1,62 +1,283 @@
 import { Injectable } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { CitizenData } from './CitizenData';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CitizenDataService {
-  searchResults: Array<CitizenData> = [];
+  private searchResults: Array<CitizenData> = [
+    {
+      id: '204466783',
+      firstName: 'Hadas',
+      lastName: 'Dahan',
+      address: 'Harakefet 18',
+      city: 'Holon',
+      vote: true
+    },
+    {
+      id: '204466700',
+      firstName: 'Haim',
+      lastName: 'Cohen',
+      address: 'Harimon 5',
+      city: 'Haifa',
+      vote: false
+    },
+    {
+      id: '302884648',
+      firstName: 'David',
+      lastName: 'Levy',
+      address: 'Hahadas Ish 2',
+      city: 'Holon',
+      vote: true
+    },
+    {
+      id: '121212123',
+      firstName: 'shmuel',
+      lastName: 'Moshkowitz',
+      address: 'Hashaked 12',
+      city: 'Haifa',
+      vote: true
+    },
+    {
+      id: '258963145',
+      firstName: 'Yosef',
+      lastName: 'Madlal',
+      address: 'Hazait 2',
+      city: 'Holon',
+      vote: false
+    },
+    {
+      id: '053698563',
+      firstName: 'Tirza',
+      lastName: 'Bitan',
+      address: 'Hatamar 11',
+      city: 'Haifa',
+      vote: true
+    },
+    {
+      id: '204466785',
+      firstName: 'Tirza',
+      lastName: 'Grudthinsky',
+      address: 'Yehuda Halevy 18',
+      city: 'Haifa',
+      vote: true
+    },
+    {
+      id: '3256981',
+      firstName: 'Brachi',
+      lastName: 'Guvlayov',
+      address: 'Hazon Ish 12',
+      city: 'Haifa',
+      vote: false
+    },
+    {
+      id: '125689784',
+      firstName: 'Chana',
+      lastName: 'Israeliyan',
+      address: 'Ben Zakai 2',
+      city: 'Bnei Brack',
+      vote: false
+    },
+    {
+      id: '984523618',
+      firstName: 'Keyla',
+      lastName: 'Gaukchman',
+      address: 'Harav Shach 3',
+      city: 'Jerusalem',
+      vote: true
+    },
+    {
+      id: '481569237',
+      firstName: 'Miryam',
+      lastName: 'Singemann',
+      address: 'Rabenu Tam 2',
+      city: 'Tel Aviv',
+      vote: true
+    },
+    {
+      id: '857485747',
+      firstName: 'Rachel',
+      lastName: 'Zimerman',
+      address: 'Hasomer 2',
+      city: 'Rishon LekZion',
+      vote: false
+    },
+    {
+      id: '968574856',
+      firstName: 'Zvia',
+      lastName: 'Abutbul',
+      address: 'El Enkawe 12',
+      city: 'Ashdod',
+      vote: false
+    },
+    {
+      id: '748514251',
+      firstName: 'Chani',
+      lastName: 'Shmueli',
+      address: 'Genichovsky 1',
+      city: 'Bat Yam',
+      vote: false
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    },
+    {
+      id: '363636363',
+      firstName: 'Michael',
+      lastName: 'Ben-Porat',
+      address: 'Povarsky 2',
+      city: 'Ashklon',
+      vote: true
+    }
+  ];
+  private subject: BehaviorSubject<CitizenData[]> = new BehaviorSubject([]);
+  private initialBallotState: boolean = true;
+  private subjectBallotState: BehaviorSubject<boolean> = new BehaviorSubject(
+    this.initialBallotState
+  );
   chooseUpdateVoter: CitizenData;
-  ballotState: boolean;
   constructor() {
-    this.searchResults = [
-      {id: '204466783', firstName: 'Hadas', lastName: 'Dahan', address: 'Harakefet 18', city: 'Holon', vote: true},
-      {id: '204466700', firstName: 'Haim', lastName: 'Cohen', address: 'Harimon 5', city: 'Haifa', vote: false},
-      {id: '302884648', firstName: 'David', lastName: 'Levy', address: 'Hahadas Ish 2', city: 'Holon', vote: true},
-      {id: '121212123', firstName: 'shmuel', lastName: 'Moshkowitz', address: 'Hashaked 12', city: 'Haifa', vote: true},
-      {id: '258963145', firstName: 'Yosef', lastName: 'Madlal', address: 'Hazait 2', city: 'Holon', vote: false},
-      {id: '053698563', firstName: 'Tirza', lastName: 'Bitan', address: 'Hatamar 11', city: 'Haifa', vote: true},
-      {id: '204466785', firstName: 'Tirza', lastName: 'Grudthinsky', address: 'Yehuda Halevy 18', city: 'Haifa', vote: true},
-      {id: '3256981', firstName: 'Brachi', lastName: 'Guvlayov', address: 'Hazon Ish 12', city: 'Haifa', vote: false},
-      {id: '125689784', firstName: 'Chana', lastName: 'Israeliyan', address: 'Ben Zakai 2', city: 'Bnei Brack', vote: false},
-      {id: '984523618', firstName: 'Keyla', lastName: 'Gaukchman', address: 'Harav Shach 3', city: 'Jerusalem', vote: true},
-      {id: '481569237', firstName: 'Miryam', lastName: 'Singemann', address: 'Rabenu Tam 2', city: 'Tel Aviv', vote: true},
-      {id: '857485747', firstName: 'Rachel', lastName: 'Zimerman', address: 'Hasomer 2', city: 'Rishon LekZion', vote: false},
-      {id: '968574856', firstName: 'Zvia', lastName: 'Abutbul', address: 'El Enkawe 12', city: 'Ashdod', vote: false},
-      {id: '748514251', firstName: 'Chani', lastName: 'Shmueli', address: 'Genichovsky 1', city: 'Bat Yam', vote: false},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true},
-      {id: '363636363', firstName: 'Michael', lastName: 'Ben-Porat', address: 'Povarsky 2', city: 'Ashklon', vote: true}];
-      this.ballotState=true;
+    let savedData = localStorage.getItem('data');
+    if (!savedData) {
+      localStorage.setItem('data', JSON.stringify(this.searchResults));
+      this.subject.next(this.searchResults);
+
+    } else {
+      this.subject.next(JSON.parse(savedData));
+    }
   }
   getSearchResults() {
     return this.searchResults;
-    }
-  setChooseUpdateVoter(citizenData: CitizenData){
-    this.chooseUpdateVoter=citizenData;
   }
-  getChooseUpdateVoter(){
+  setChooseUpdateVoter(citizenData: CitizenData) {
+    this.chooseUpdateVoter = citizenData;
+  }
+  getChooseUpdateVoter() {
     return this.chooseUpdateVoter;
   }
-  updateVoter(citizenDataId:string){
-    this.searchResults.find(citizenData => citizenData.id === citizenDataId).vote=true;
+  updateVoter(citizenDataId: string) {
+    this.searchResults.find(
+      citizenData => citizenData.id === citizenDataId
+    ).vote = true;
+    localStorage.setItem('data', JSON.stringify(this.searchResults));
+    this.subject.next(this.searchResults);
   }
-  updateBallotState() {
-    this.ballotState=false;
-  }
+  /*updateBallotState() {
+    this.initialBallotState = false;
+  }*/
   getBallotState() {
-    return this.ballotState;
+    return this.initialBallotState;
+  }
+  get data$(): Observable<CitizenData[]> {
+    return this.subject.asObservable();
+  }
+  get ballotState$(): Observable<boolean> {
+    return this.subjectBallotState.asObservable();
+  }
+  set BallotState(ballotState: boolean) {
+    this.subjectBallotState.next(ballotState);
   }
 }
--- Elections/src/app/search-results/search-results.component.ts
+++ Elections/src/app/search-results/search-results.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { CitizenData } from '../CitizenData';
 import { CitizenDataService } from '../citizen-data.service';
 import { FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.css']
 })
-export class SearchResultsComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'address', 'city', 'vote'];
+export class SearchResultsComponent implements OnInit, OnDestroy {
+private subs: Subscription;
+  displayedColumns: string[] = [
+    'id',
+    'firstName',
+    'lastName',
+    'address',
+    'city',
+    'vote'
+  ];
   dataSource: MatTableDataSource<CitizenData>;
   idFilter: FormControl;
   firstNameFilter: FormControl;
@@ -20,15 +29,25 @@ export class SearchResultsComponent implements OnInit {
   addressFilter: FormControl;
   cityFilter: FormControl;
   voteFilter: FormControl;
-  filteredValues: { id: string; firstName: string; lastName: string; address: string; city: string; vote: string; };
+  filteredValues: {
+    id: string;
+    firstName: string;
+    lastName: string;
+    address: string;
+    city: string;
+    vote: string;
+  };
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
   constructor(private citizenDataService: CitizenDataService) {
-    this.dataSource = new MatTableDataSource(citizenDataService.getSearchResults());
+    // this.dataSource = new MatTableDataSource(citizenDataService.getSearchResults());
   }
 
   ngOnInit() {
+    this.subs =  this.citizenDataService.data$.subscribe(data => {
+      this.dataSource = new MatTableDataSource(data);
+    });
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
     this.idFilter = new FormControl();
@@ -37,52 +56,95 @@ export class SearchResultsComponent implements OnInit {
     this.addressFilter = new FormControl();
     this.cityFilter = new FormControl();
     this.voteFilter = new FormControl();
-    this.filteredValues = { id: '', firstName: '', lastName: '', address: '', city: '', vote: ''};
-    this.idFilter.valueChanges.subscribe((idFilterValue) => {
+    this.filteredValues = {
+      id: '',
+      firstName: '',
+      lastName: '',
+      address: '',
+      city: '',
+      vote: ''
+    };
+    this.idFilter.valueChanges.subscribe(idFilterValue => {
       this.filteredValues.id = idFilterValue;
       this.dataSource.filter = JSON.stringify(this.filteredValues);
-      });
-    this.firstNameFilter.valueChanges.subscribe((firstNameFilterValue) => {
-        this.filteredValues.firstName = firstNameFilterValue;
-        this.dataSource.filter = JSON.stringify(this.filteredValues);
-      });
-    this.lastNameFilter.valueChanges.subscribe((lastNameFilterValue) => {
-        this.filteredValues.lastName = lastNameFilterValue;
-        this.dataSource.filter = JSON.stringify(this.filteredValues);
-      });
-    this.addressFilter.valueChanges.subscribe((addressFilterValue) => {
-        this.filteredValues.address = addressFilterValue;
-        this.dataSource.filter = JSON.stringify(this.filteredValues);
-      });
-    this.cityFilter.valueChanges.subscribe((lastNameFilterValue) => {
-        this.filteredValues.city = lastNameFilterValue;
-        this.dataSource.filter = JSON.stringify(this.filteredValues);
-      });
-    this.voteFilter.valueChanges.subscribe((lastNameFilterValue) => {
-        this.filteredValues.vote = lastNameFilterValue;
-        this.dataSource.filter = JSON.stringify(this.filteredValues);
-      });
+    });
+    this.firstNameFilter.valueChanges.subscribe(firstNameFilterValue => {
+      this.filteredValues.firstName = firstNameFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
+    this.lastNameFilter.valueChanges.subscribe(lastNameFilterValue => {
+      this.filteredValues.lastName = lastNameFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
+    this.addressFilter.valueChanges.subscribe(addressFilterValue => {
+      this.filteredValues.address = addressFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
+    this.cityFilter.valueChanges.subscribe(lastNameFilterValue => {
+      this.filteredValues.city = lastNameFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
+    this.voteFilter.valueChanges.subscribe(lastNameFilterValue => {
+      this.filteredValues.vote = lastNameFilterValue;
+      this.dataSource.filter = JSON.stringify(this.filteredValues);
+    });
     this.dataSource.filterPredicate = this.customFilterPredicate();
+  }
+  ngOnDestroy(): void {
+   this.subs.unsubscribe();
   }
   customFilterPredicate() {
     const myFilterPredicate = (data: CitizenData, filter: string): boolean => {
-                              const searchString = JSON.parse(filter);
-                              return data.id.toString().trim().indexOf(searchString.id) !== -1 &&
-                                     data.firstName.toString().trim().toLowerCase().indexOf(searchString.firstName.toLowerCase()) !== -1 &&
-                                     data.lastName.toString().trim().toLowerCase().indexOf(searchString.lastName.toLowerCase()) !== -1 &&
-                                     data.address.toString().trim().toLowerCase().indexOf(searchString.address.toLowerCase()) !== -1 &&
-                                     data.city.toString().trim().toLowerCase().indexOf(searchString.city.toLowerCase()) !== -1 &&
-                                     data.vote.toString().trim().toLowerCase().indexOf(searchString.vote.toLowerCase()) !== -1; };
+      const searchString = JSON.parse(filter);
+      return (
+        data.id
+          .toString()
+          .trim()
+          .indexOf(searchString.id) !== -1 &&
+        data.firstName
+          .toString()
+          .trim()
+          .toLowerCase()
+          .indexOf(searchString.firstName.toLowerCase()) !== -1 &&
+        data.lastName
+          .toString()
+          .trim()
+          .toLowerCase()
+          .indexOf(searchString.lastName.toLowerCase()) !== -1 &&
+        data.address
+          .toString()
+          .trim()
+          .toLowerCase()
+          .indexOf(searchString.address.toLowerCase()) !== -1 &&
+        data.city
+          .toString()
+          .trim()
+          .toLowerCase()
+          .indexOf(searchString.city.toLowerCase()) !== -1 &&
+        data.vote
+          .toString()
+          .trim()
+          .toLowerCase()
+          .indexOf(searchString.vote.toLowerCase()) !== -1
+      );
+    };
     return myFilterPredicate;
   }
-  dataChanging(){
-    if (this.dataSource.filteredData.length===1 && this.dataSource.filteredData[0].vote===false
-      &&this.citizenDataService.getBallotState())
-    {
-      (<HTMLInputElement> document.getElementById("updateVoterBtn")).disabled = false;
-      this.citizenDataService.setChooseUpdateVoter(this.dataSource.filteredData[0]);
-    }
-    else
-      (<HTMLInputElement> document.getElementById("updateVoterBtn")).disabled = true;
+  dataChanging() {
+    if (
+      this.dataSource.filteredData.length === 1 &&
+      this.dataSource.filteredData[0].vote === false &&
+      this.citizenDataService.getBallotState()
+    ) {
+      (<HTMLInputElement>(
+        document.getElementById('updateVoterBtn')
+      )).disabled = false;
+      this.citizenDataService.setChooseUpdateVoter(
+        this.dataSource.filteredData[0]
+      );
+    } else
+      (<HTMLInputElement>(
+        document.getElementById('updateVoterBtn')
+      )).disabled = true;
   }
 }
